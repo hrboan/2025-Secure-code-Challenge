61af3c6015b1ef7e19c274dd234d01952fe719f6
hrboan <jjen8853@naver.com>
2025-10-09 22:55:26 +0900

    Update Pop-Up.py

--- Pop-Up.py
+++ Pop-Up.py
@@ -12,8 +12,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
-
+) 
 
 @app.get("/fragment", response_class=HTMLResponse)
 async def popup_fragment(url: str = "", score: str = "", decision: str = ""):
